85fccefd5ea9fbff791232c4a41372d91575b260
mimiMonads <mimi@monads.dev>
2023-01-21 22:11:06 +0000

    adding support  to mime types

--- optimizer/response1.ts
+++ optimizer/response1.ts
@@ -2,6 +2,7 @@ import { funRouterOptions } from "../types.ts";
 import { ObjectRawCommonRequest, ObjectRawResponseCommon } from "./types.ts";
 import checker from "./checker.ts";
 import aComposer from "./aComposer.ts";
+import mime from "../components/util/mime.ts";
 
 export default (o?: funRouterOptions) =>
 (f: ObjectRawResponseCommon | ObjectRawCommonRequest) =>
@@ -13,21 +14,27 @@ export default (o?: funRouterOptions) =>
         ? "status" in f || "header" in f
           ? ((h: ResponseInit) => async (r: Request) =>
             new Response(await f.f(c(r)) as BodyInit, h))(
-              (new Function(
-                `{${"header" in f ? "header:" + f.header + "," : ""}${
-                  "status" in f ? "status:" + f.status + "," : ""
-                }}`,
-              ))(),
+              {
+                headers: "header" in f 
+                  ? typeof f.header === "string" 
+                    ? { 'Content-Type' : mime.find( x => x[0] === f.header)![1]}  
+                    : f.header as Record<string,string>
+                  : { 'Content-Type' : 'text/plain'} ,
+                status: "status" in f ? f.status : 200
+              }
             )
           : async (r: Request) => new Response(await f.f(c(r)) as BodyInit)
         : "status" in f || "header" in f
         ? ((h: ResponseInit) => (r: Request) =>
-          new Response(f.f(c(r)) as BodyInit, h))(
-            (new Function(
-              `{${"header" in f ? "header:" + f.header + "," : ""}${
-                "status" in f ? "status:" + f.status + "," : ""
-              }}`,
-            ))(),
+              new Response(f.f(c(r)) as BodyInit, h)
+              )({
+                headers: "header" in f 
+                  ? typeof f.header === "string" 
+                    ? { 'Content-Type' : mime.find( x => x[0] === f.header)![1]}  
+                    : f.header as Record<string,string>
+                  : { 'Content-Type' : 'text/plain'} ,
+                status: "status" in f ? f.status : 200
+              }
           )
         : (r: Request) => new Response(f.f(c(r)) as BodyInit))(
         aComposer(o)(f as ObjectRawResponseCommon)(el),
