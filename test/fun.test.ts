5f81bb374c63ccdd81f7523fc87c49e1f18dab99
mimiMonads <mimi@monads.dev>
2023-01-22 23:58:38 +0000

    re-organising tests

--- test/fun.test.ts
+++ test/fun.test.ts
@@ -4,140 +4,76 @@ import fun from "../fun.ts";
 
 Deno.test(
   "main",
-  async (_) =>
+   (_) =>
+  ( 
+    async f => 
     assertEquals(
-      await fun()(paths)(new Request("http://localhost:8080/")).text(),
-      "GET:main",
-    ),
-);
+      [
+        await f(new Request("http://localhost:8080/")).text(),
+        await f(new Request("http://localhost:8080/test")).text(),
+        await f(new Request("http://localhost:8080/test/")).text(),
+        await f(new Request("http://localhost:8080/", { method: "POST" }),).text(),
+        await f(new Request("http://localhost:8080/", { method: "HEAD" }),).text(),
+        await f(new Request("http://localhost:8080/", { method: "DELETE" }), ).text(),
+        await fun()(paths)(new Request("http://localhost:8080/test/1/2/")).text(),
+        f(new Request("http://localhost:8080/notFound")).status,
+        f(new Request("http://localhost:8080/notFound", { method: "BAD_METHOD" })).status,
+      ],
+      [
+        "GET:main" ,
+        "GET:test" ,
+        "GET:test/",   
+        "POST:main",
+        "HEAD:main",
+        "DELETE:main",
+        "GET:test/:id/:name/",
+        404,
+        405
+      ]
+      ,
+    )
+  )(
+    fun()(paths)
+  )
+)
 
 Deno.test(
   "main",
-  async (_) =>
+   (_) =>
+  ( 
+    async f => 
     assertEquals(
-      await fun()(paths)(new Request("http://localhost:8080/test")).text(),
-      "GET:test",
-    ),
-);
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun()(paths)(new Request("http://localhost:8080/test/")).text(),
-      "GET:test/",
-    ),
-);
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun()(paths)(
-        new Request("http://localhost:8080/", { method: "POST" }),
-      ).text(),
-      "POST:main",
-    ),
-);
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun()(paths)(
-        new Request("http://localhost:8080/", { method: "HEAD" }),
-      ).text(),
-      "HEAD:main",
-    ),
-);
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun()(paths)(new Request("http://localhost:8080/test/")).text(),
-      "GET:test/",
-    ),
-);
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun({
-      })(paths)(new Request("http://localhost:8080/notFound")).status,
-      404,
-    ),
-);
+      [
+        await f(new Request("http://localhost:8080/")).text(),
+        await f(new Request("http://localhost:8080/test")).text(),
+        await f(new Request("http://localhost:8080/test/")).text(),
+        await f(new Request("http://localhost:8080/", { method: "POST" }),).text(),
+        await f(new Request("http://localhost:8080/", { method: "HEAD" }),).text(),
+        await f(new Request("http://localhost:8080/", { method: "DELETE" }), ).text(),
+        await fun()(paths)(new Request("http://localhost:8080/test/1/2/")).text(),
+        f(new Request("http://localhost:8080/notFound")).status,
+        f(new Request("http://localhost:8080/notFound", { method: "BAD_METHOD" })).status,
+      ],
+      [
+        "GET:main" ,
+        "GET:test" ,
+        "GET:test/",   
+        "POST:main",
+        "HEAD:main",
+        "DELETE:main",
+        "GET:test/:id/:name/",
+        404,
+        405
+      ]
+      ,
+    )
+  )(
+    fun({
+      hasName: "http://localhost:8080/",
+    })(paths)
+  )
+)
 
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun()(paths)(
-        new Request("http://localhost:8080/notFound", { method: "BAD_METHOD" }),
-      ).status,
-      405,
-    ),
-);
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun()(paths)(new Request("http://localhost:8080/notFound/")).status,
-      404,
-    ),
-);
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun()(paths)(
-        new Request("http://localhost:8080/", { method: "DELETE" }),
-      ).text(),
-      "DELETE:main",
-    ),
-);
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun({
-      })(paths)(new Request("http://localhost:8080/", { method: "DELETE" }))
-        .text(),
-      "DELETE:main",
-    ),
-);
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun()(paths)(new Request("http://localhost:8080/test/1/2/")).text(),
-      "GET:test/:id/:name/",
-    ),
-);
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun({
-      })(paths)(new Request("http://localhost:8080/test/1/2/")).text(),
-      "GET:test/:id/:name/",
-    ),
-);
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun({
-        hasName: "http://localhost:8080/",
-      })(paths)(new Request("http://localhost:8080/")).text(),
-      "GET:main",
-    ),
-);
 
-Deno.test(
-  "main",
-  async (_) =>
-    assertEquals(
-      await fun({
-        hasName: "http://localhost:8080/",
-      })(paths)(new Request("http://localhost:8080/test/1/2/")).text(),
-      "GET:test/:id/:name/",
-    ),
-);
+
+
