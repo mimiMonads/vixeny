f84cc1c58558089b1a4c1987ec64e55f6f76fd1b
mimiMonads <mimi@monads.dev>
2023-02-03 22:38:26 +0000

    fic small error

--- builder/atlas/map.ts
+++ builder/atlas/map.ts
@@ -8,20 +8,20 @@ export default (c: string[][][]) =>(
     )(
         el
         .map(
-             x => x.reduce( (acc,y) => y + acc)
+             x => x.reduce( (acc,y) => y + acc) 
         )
-        .map( x => x === 0 ? 1 : x)
+        .map( x => x + 1)
         .map(
             (_,i,a) => i === 0
                 ? 0
-                : a.slice(0,i).reduce( (acc,x) => acc + x  ) - 1
+                : a.slice(0,i).reduce( (acc,x) => acc + x, -2  ) 
         )
     )
 )(
     c.map(
         x => x
         .map( (y) => y.length)
-        .map( (y,i) => x.slice(0,i+1).reduce( (acc,z) => z.length + acc , 0) - y)
+        .map( (y,i) => x.slice(0,i+1).reduce( (acc,z) => z.length + acc , 0) - y) 
     )
 )
 
