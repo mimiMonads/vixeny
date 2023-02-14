6548b5cf58816461478e53779f25ee136c56792d
mimiMonads <mimi@monads.dev>
2023-02-14 13:14:15 +0000

    crc

---
+++ components/signer/crcTable.ts
@@ -0,0 +1,8 @@
+export default (sed=0xEDB88320) => (() => Array.from(
+    {length:256},
+    (_,i) => Array.from(
+        {length: 8},
+    ).reduce(
+        (acc:number) => ((acc&1) ? (sed ^ (acc >>> 1)) : (acc >>> 1) === 0 ? 0xFFFFFFF :(acc >>> 1)) , i
+    )
+))()
