8a80e6cd61c9e34960aafe1f7894908387d913f9
mimiMonads <mimi@monads.dev>
2023-02-16 15:03:18 +0000

    fmt

--- components/signer/signer.ts
+++ components/signer/signer.ts
@@ -8,23 +8,23 @@ export default async (seed?: string) =>
         (v) => (s: string) =>
           (
             (p) =>
-              (s.length === 8 
-                ? s : s.length > 8 
-                  ? s.slice(0, 8) 
-                    : s) + "." +
+              (s.length === 8
+                ? s
+                : p.map((x) => String.fromCharCode(x)).join("")) +
+              "." +
               v[arp[0](p)] + v[arp[1](p)] + v[arp[2](p)] +
-              v[arp[3](p)] +
-              v[arp[4](p)] + v[arp[5](p)] + v[arp[6](p)] + v[arp[7](p)]
+              v[arp[3](p)] + v[arp[4](p)] + v[arp[5](p)] +
+              v[arp[6](p)] + v[arp[7](p)]
           )(
             [
-              s.charCodeAt(0),
-              s.charCodeAt(1),
-              s.charCodeAt(2),
-              s.charCodeAt(3),
-              s.charCodeAt(4),
-              s.charCodeAt(5),
-              s.charCodeAt(6),
-              s.charCodeAt(7),
+              s[0] ? s.charCodeAt(0) : 59,
+              s[1] ? s.charCodeAt(1) : 59,
+              s[2] ? s.charCodeAt(2) : 59,
+              s[3] ? s.charCodeAt(3) : 59,
+              s[4] ? s.charCodeAt(4) : 59,
+              s[5] ? s.charCodeAt(5) : 59,
+              s[6] ? s.charCodeAt(6) : 59,
+              s[7] ? s.charCodeAt(7) : 59,
             ],
           )
       )(
