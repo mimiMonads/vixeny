f98a1c41236dcc897f0ee9d0390601ca893eadcd
mimiMonads <mimi@monads.dev>
2023-02-28 22:23:12 +0000

    pre v0.0.3.0

--- components/signer/verifier.ts
+++ components/signer/verifier.ts
@@ -4,17 +4,24 @@ import { SignVerifyOptions } from "./types.ts";
 
 export default async (seed: SignVerifyOptions) => (
     ar => (
-       p => (s:string) =>
+       p => (
+        f => 
+            seed.expires === true
+            ?   (s:string) => 
+                Number(s.slice(0,13)) > Date.now() && f(s)
+            : f
+       ) (
+       (ar => (p:string[])=> ((s:string) =>
        s.length >= 17
         ? (
             m => 
             s
-        .slice(0, m)
+        .slice(0, m -1 )
         .split("")
         .map( x => x.charCodeAt(0))
         .every( (x,i,a)  => 
         i < 7 
-            ? p[ar[i%8]([a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7]])] === s[m +  + 1 + i]
+            ? p[ar[i%8]([a[0],a[1],a[2],a[3],a[4],a[5],a[6],a[7]])] === s[m + i]
             : p[
                 ar[i%8]([
                     a[(i-7)],
@@ -25,13 +32,14 @@ export default async (seed: SignVerifyOptions) => (
                     a[(i-2)],
                     a[(i-1)],
                     x,
-                ])] === s[m + 1 + i]
+                ])] === s[m + i]
         )
         )(
-            s.length / 2 >> 0
+            (s.length / 2 >> 0) + 1
         )
 
-        : false
+        : false)) (ar)(p)
+       )
     )(
        [...validChar]
     )
