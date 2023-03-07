209afc103d936527d20ebad78d231d3533e9b049
mimiMonads <mimi@monads.dev>
2023-03-07 11:56:15 +0000

    prototype to introduce wildcards

--- builder/composer/wild.ts
+++ builder/composer/wild.ts
@@ -1,29 +1,43 @@
-import { funRouterOptions, RouteTypes } from "../../types.ts";
-
-// (
-//     ((a: string[]) => ((o) => (s: string) => o.indexOf(s))(a.map((x) => x)))(
-//       atlas[0],
-//     )
+import { Atlas, funRouterOptions } from "../../types.ts";
 
 export default 
- (_o?: funRouterOptions) => (max: number) => (ar: RouteTypes[]) =>
+ (_o?: funRouterOptions) => (max: number) => (atlas: Atlas) =>  (
+    me => (
+        methodMap => (r:Request) => (
+            meth => meth !== -1
+                ? methodMap[0](r.url)
+                : -1
+        )(
+            me(r.url)
+        )
+    ) (
+        atlas[0].map( method => 
+            (
+                ar =>  
+                (
+                    (f) =>  f
+                )(
+                    ((ar: string[]) =>
+                    (new Function(`return s=>${
+                        ar.reduceRight(
+                        (acc, v, i) => `s.indexOf("${v}")===0?${i + max - 2}:` + acc,
+                        "-1",
+                        )
+                    }`))() as (s: string) => number)(ar),
+                )
+            )(
+                atlas[0].filter(
+                    x => x[0] !== method 
+                )
+                .map(
+                    x => x[3]
+                )
+            )
+        )
+    )
+ )(
+    ((a: string[]) => ((o) => (s: string) => o.indexOf(s))(a.map((x) => x)))(
+      atlas[0],
+    )
+ )
 
-
-// export default (_o?: funRouterOptions) => (max: number) => (ar: RouteTypes[]) =>
-//       (
-//         (nar) =>
-//           (
-//             (f) => f(nar)
-//           )(
-//             (ar: string[]) =>
-//               (new Function(`return s=>${
-//                 ar.reduceRight(
-//                   (acc, v, i) => `s.indexOf("${v}")===0?${i + max - 2}:` + acc,
-//                   "-1",
-//                 )
-//               }`))() as (s: string) => number,
-//           )
-//       )(
-//         ar.map(([_, a]) => a.slice(1))
-//           .sort((a, b) => b.length - a.length),
-//       );
