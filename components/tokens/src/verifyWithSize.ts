1528e1fb2ceb087d7583bedf6fddf0eda04a69b3
mimiMonads <mimi@monads.dev>
2023-07-17 10:30:29 +0100

    fixing range

--- components/tokens/src/verifyWithSize.ts
+++ components/tokens/src/verifyWithSize.ts
@@ -1,4 +1,4 @@
 export default (n: number) =>
- new Function(
-    ` return ar => p=> s=>( a=>${Array.from({length: n}, (_,i)=> `p[ar[${i%8}]([${Array.from({length: 8}, (_,j)=> `a[${(n-7+i+j)%n}]`).join(",")}])] ===  s[${i+1+n}]` ).join('&&')})([${Array.from({length: n}, (_,i) => `s.charCodeAt(${i})`).join(",")}]) `
- )()
+  new Function(
+    ` return ar => p=> s=>( a=> s.length===${(n * 2) + 1}  &&${Array.from({ length: n }, (_, i) => `p[ar[${i % 8}]([${Array.from({ length: 8 }, (_, j) => `a[${(n - 7 + i + j) % n}]`).join(",")}])] ===  s[${i + 1 + n}]`).join('&&')})([${Array.from({ length: n }, (_, i) => `s.charCodeAt(${i})`).join(",")}]) `
+  )()
