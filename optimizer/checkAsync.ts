71c890de8483ee28fa033126247382453c62cf06
mimiMonads <mimi@monads.dev>
2023-08-04 23:44:22 +0100

    checkAsync(tested)

--- optimizer/checkAsync.ts
+++ optimizer/checkAsync.ts
@@ -1,21 +1,26 @@
 // deno-lint-ignore-file
+
 import { ResolveOptions } from "./resolve/types.ts";
 import { ObjectRawResponseCommon, ObjectRawCommonRequest } from "./types.ts";
 
-export default (f: ObjectRawResponseCommon) =>
-  (y: (arg0: (y: any) => any) => any) =>
-    ((x) => x(x))((x: (arg0: any) => ({ (arg0: any): any; new(): any })) =>
-      y((y: any) => x(x)(y))
-    )(
 
-      y =>
-        (f.f.constructor.name === "AsyncFunction")
+type RecFunc = (f: ObjectRawResponseCommon) => boolean;
+
+export default 
+((f: (x: RecFunc) => RecFunc) => 
+  ((x: (arg: (x: any) => any) => any) => x(x))(
+    (x: (arg: any) => any) => 
+      f((y: ObjectRawResponseCommon) => x(x)(y))
+  )
+)(
+  (solver: RecFunc) => (f: ObjectRawResponseCommon) => 
+    f.f.constructor.name === "AsyncFunction"
+    ? true
+    : f.f.constructor.name === "Function" && typeof f.resolve == "undefined"
+      ? false
+      : !Array.isArray(f.resolve) && f.resolve && f.resolve.f.constructor.name === "AsyncFunction"
+        ? true
+        : "resolve" in f && Array.isArray(f.resolve) && f.resolve.some(x => solver(x as unknown as ObjectRawResponseCommon))
           ? true
-          : (f.f.constructor.name === "Function" && typeof f.resolve == "undefined")
-            ? false
-            : (!Array.isArray(f.resolve) && f.resolve && f.resolve.f.constructor.name === "AsyncFunction")
-              ? true
-              : ("resolve" in f && Array.isArray(f.resolve) && f.resolve.every(x => y({ ...x })))
-                ? true
-                : false
-    ) as boolean
+          : false
+) as unknown as (f: ObjectRawResponseCommon) => boolean;
