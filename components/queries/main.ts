e21ba1698101a17aa8b9e2857cdbb0a8133a8ba3
mimiMonads <mimi@monads.dev>
2023-08-24 11:00:13 +0100

    better inferring

--- components/queries/main.ts
+++ components/queries/main.ts
@@ -2,6 +2,14 @@ import { funRouterOptions } from "../../types.ts";
 import common from "./common.ts";
 import elements from "./elements.ts";
 import { ObjectRawResponseCommon } from "../../optimizer/types.ts";
+import filter from "./filter.ts";
 export default (o?: funRouterOptions) =>
   (f: ObjectRawResponseCommon) =>
-    f && "query" in f && f.query?.only ? new Function(`return ${elements(f.query.only)}`)() : new Function(`return ${common(o)(f)}`)();
+    f && "query" in f && f.query?.only 
+    ? new Function(`return ${elements(f.query.only)}`)() 
+    : (
+      only => 
+        only.length > 0 
+         ? new Function(`return ${elements(only)}`)() 
+         : new Function(`return ${common(o)(f)}`)()
+    )(filter(f.f.toString().split(" "))("query"))
