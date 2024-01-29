3174eb243cdc4a3c52cb0e0e6a066dfbe568913e
mimiMonads <mimi@monads.dev>
2024-01-29 10:36:12 +0000

    for null cases

--- components/http/src/cookieToToken/cookieToTokenGets.ts
+++ components/http/src/cookieToToken/cookieToTokenGets.ts
@@ -3,17 +3,19 @@ import { SupportedKeys } from "../framework/optimizer/types.ts";
 
 export default (secret: SupportedKeys) => (name: string) =>
   (
-    (sha256) => (c: string) =>
+    (sha256) => (c: string | null) =>
       (
         (p) =>
+          p !== -1?
           sha256(
             c.slice(
               p + name.length + 1,
               (c.indexOf(",", p) + 1 || c.length + 1) - 1,
             ),
           )
+          : null
       )(
-        c.indexOf(name + "="),
+         typeof c === 'string' ? c.indexOf(name + "=") : -1,
       )
   )(
     verifySha256()(secret),
