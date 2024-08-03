c236da1812d63841ce1bea85dd76168afd65942f
mimiMonads <mimi@monads.dev>
2024-08-03 10:41:22 +0100

    Improving comments

--- src/staticFiles/staticFileTools.ts
+++ src/staticFiles/staticFileTools.ts
@@ -9,14 +9,21 @@ import mime from "../util/mime.ts";
 import bunSyncCheckDir from "./transverseFiles.ts";
 
 export default {
+  // Correctly joins string that starts with `/` or ends with `/`
   join: (base: string) => (target: string): string =>
     base.endsWith("/") ? base + target : base + "/" + target,
 
+  // Checks 
   mimeForm: (f: fileServerPetition<any>) =>
     "mime" in f && f.mime === false
       ? []
-      : "extra" in f
-      ? mime.concat(f.extra as [string, string][])
+      : f.extra
+        // Creates a map of mime and overwrites the extra keys if already exist
+      ? f.extra.reduce( ( map , v) =>  ( 
+        map.set(v[0], v[1]),
+        map
+      ) ,  new Map<string,string>(mime))
+
       : mime,
 
   getMime: (mimes: [string, string][]) => (ext: string): string =>
@@ -24,27 +31,33 @@ export default {
 
   removeExtension: (f: fileServerPetition<any>) => (petitions: Petition[]) =>
     f.removeExtensionOf && Array.isArray(f.removeExtensionOf)
+      // Maps over all the petitions
       ? petitions.map(
         (x) =>
           (
-            (element) =>
-              element
+            (ifExtension) =>
+              ifExtension
                 ? {
                   ...x,
-                  path: x.path.slice(0, x.path.length - element.length),
+                  path: x.path.slice(0, x.path.length - ifExtension.length),
                 }
                 : x
           )(
-            (f.removeExtensionOf as unknown as defaultMime[]).find((y) =>
+            // Checks if this petition has any of the extension and returning the first instance
+            (f.removeExtensionOf as defaultMime[]).find((y) =>
               x.path.includes(y)
             ),
           ),
+          
       ) as Petition[]
       : petitions,
 
   rectify: (path: string) =>
+    // Add functions to check modify the `path`
     [
+      // Ensures a `/` ad the end
       (x: string) => x[x.length - 1] !== "/" ? x + "/" : x,
+      // Ensures a `.` at the start
       (x: string) => x[0] === "." ? x : "." + x,
     ].reduceRight((acc, x) => x(acc), path),
 
@@ -59,22 +72,23 @@ export default {
     (o?: FunRouterOptions<any>) =>
     (checks: StaticFilePluginExtensions<any>) =>
     (root: string) =>
-    (x: string) =>
+    (path: string) =>
     (name: string) =>
       "r" in checks
         ? checks!.r(
           {
             root: root,
-            path: x,
+            path,
             o: o,
-            relativeName: root.slice(1, -1) + x.slice(name.length - 1),
+            relativeName: root.slice(1, -1) + path.slice(name.length - 1),
           },
         )
         : checks!.f(
           {
             root: root,
-            path: x,
-            relativeName: root.slice(1, -1) + x.slice(name.length - 1),
+            path,
+            o: o,
+            relativeName: root.slice(1, -1) + path.slice(name.length - 1),
           },
         ),
 };
